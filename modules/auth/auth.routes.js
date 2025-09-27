a4211de773374a7f424c684c115b1d29299c76cd
LordKode <richard.ati@epitech.eu>
2025-09-27 03:11:29 +0100

    chore(auth): add authentification middleware to some routes

--- modules/auth/auth.routes.js
+++ modules/auth/auth.routes.js
@@ -5,12 +5,18 @@ const { registerSchema, loginSchema } = require("../../schemas/auth.schema");
 const AuthMiddleware = require("../../middleware/auth");
 
 // Route d'inscription avec validation
-router.post("/register", AuthMiddleware.validate(registerSchema), (req, res, next) =>
-  authController.register(req, res, next)
+router.post(
+  "/register",
+  AuthMiddleware.validate(registerSchema),
+  (req, res, next) => authController.register(req, res, next)
 );
-router.post("/login", AuthMiddleware.validate(loginSchema), (req, res, next) => {
-  authController.login(req, res, next);
-});
+router.post(
+  "/login",
+  AuthMiddleware.validate(loginSchema),
+  (req, res, next) => {
+    authController.login(req, res, next);
+  }
+);
 router.get("/verify-email", (req, res, next) => {
   authController.verifyEmail(req, res, next);
 });
@@ -26,22 +32,41 @@ router.get("/verify-password-reset-token", (req, res, next) => {
 router.post("/reset-password", (req, res, next) => {
   authController.resetPassword(req, res, next);
 });
-router.put("/disabled/:userId", (req, res, next) => {
-  authController.disableUser(req, res, next);
-});
-router.put("/enable/:userId", (req, res, next) => {
-  authController.enableUser(req, res, next);
-});
-router.delete("/soft-delete/:userId", (req, res, next) => {
-  authController.softDeleteUser(req, res, next);
-});
-router.put("/restore/:userId", (req, res, next) => {
-  authController.restoreUser(req, res, next);
-});
-router.get("/users/me", (req, res, next) => {
+router.put(
+  "/disabled/:userId",
+  AuthMiddleware.authenticate,
+  AuthMiddleware.isAdmin,
+  (req, res, next) => {
+    authController.disableUser(req, res, next);
+  }
+);
+router.put(
+  "/enable/:userId",
+  AuthMiddleware.authenticate,
+  AuthMiddleware.isAdmin,
+  (req, res, next) => {
+    authController.enableUser(req, res, next);
+  }
+);
+router.delete(
+  "/soft-delete/:userId",
+  AuthMiddleware.authenticate,
+  AuthMiddleware.isAdmin,
+  (req, res, next) => {
+    authController.softDeleteUser(req, res, next);
+  }
+);
+router.put(
+  "/restore/:userId",
+  AuthMiddleware.authenticate,
+  (req, res, next) => {
+    authController.restoreUser(req, res, next);
+  }
+);
+router.get("/users/me", AuthMiddleware.authenticate, (req, res, next) => {
   authController.getProfile(req, res, next);
 });
-router.put("/users/me", (req, res, next) => {
+router.put("/users/me", AuthMiddleware.authenticate, (req, res, next) => {
   authController.updateProfile(req, res, next);
 });
 
